12de65341c18d707366f94c853d897689deb9b2c
smartdev0612 <danieldev0612@gmail.com>
2023-03-21 20:26:09 -0700

    create graphql resolvers and model

--- server.js
+++ server.js
@@ -1,7 +1,48 @@
+const path = require('path')
 const express = require('express')
+const { buildSchema } = require('graphql')
+const { graphqlHTTP } = require('express-graphql')
+
+const { loadFilesSync } = require('@graphql-tools/load-files')
+const { makeExecutalbeSchema } = require('@graphql-tools/schema')
+
+const typesArray = loadFilesSync('**/*', {
+  extensions: ['graphql'],
+})
+
+const schema = makeExecutalbeSchema({
+  typeDefs: typesArray,
+  resolvers: {
+    Query: {
+      products: async (parent) => {
+        console.log('Getting the products...')
+        const product = await Promise.resolve(parent.products)
+        return product
+      },
+      orders: (parent) => {
+        console.log('Getting orders...')
+        return parent.orders
+      },
+    },
+  },
+})
+
+const root = {
+  products: require('./products/products.model'),
+  orders: require('./orders/orders.model'),
+}
 
 const app = express()
 
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
+
 app.listen(3000, () => {
-    console.log('Running GraphQL server...')
+  console.log('Running GraphQL server...')
 })
